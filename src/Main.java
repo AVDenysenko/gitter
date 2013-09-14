f44a5f6f09cdd3c2e2f46ed13a8612c8fa7ffd14
andrew <andrew@localhost.localdomain>
2013-09-14 23:35:58 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: andrew
+ * Date: 9/14/13
+ * Time: 11:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
